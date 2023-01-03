ccbcc5695740bf6a4871b3a8c6f463628fcdbbfe
zhangxq <zzzzail@163.com>
2023-01-03 21:38:01 +0800

    1

--- golang/leetcode/p41.go
+++ golang/leetcode/p41.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 // 缺失的第一个正数
 // Link https://leetcode.cn/problems/first-missing-positive/
 func firstMissingPositive(nums []int) int {
@@ -17,4 +19,28 @@ func firstMissingPositive(nums []int) int {
 	}
 
 	return res
+}
+
+// 要求 O(n) 的时间复杂度 O(1) 的空间复杂度
+func firstMissingPositive2(nums []int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] != math.MaxInt {
+			nums[i]--
+		}
+	}
+	for i := 0; i < n; i++ {
+		// 0 <= nums[i] && nums[i] < n 因为结果在 (0, n] 所以这里我们用到的数组的下标应该是 [0, n)
+		// nums[i] != i 要是 nums[i] 已经在第 i 个位置上了，那么就不用交换了
+		// nums[i] != nums[nums[i]] 防止无限交换
+		for 0 <= nums[i] && nums[i] < n && nums[i] != i && nums[i] != nums[nums[i]] {
+			nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+		}
+	}
+	for i := 0; i < n; i++ {
+		if nums[i] != i {
+			return i + 1
+		}
+	}
+	return n + 1
 }
