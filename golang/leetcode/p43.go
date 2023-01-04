b8fcda36dff88b5492b9cd27d816437123f8621e
zhangxq <zzzzail@163.com>
2023-01-04 09:52:20 +0800

    1

--- golang/leetcode/p43.go
+++ golang/leetcode/p43.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	multiply("123", "456")
+	res := multiply("123", "456")
+	fmt.Println(res)
 }
 
 // 字符串相乘
 // Link https://leetcode.cn/problems/multiply-strings/
 func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
 	m, n := len(num1), len(num2)
 	a := make([]int, m+n)
 	for i := m - 1; i >= 0; i-- {
@@ -18,15 +23,22 @@ func multiply(num1 string, num2 string) string {
 			a[i+j+1] += int(num1[i]-'0') * int(num2[j]-'0')
 		}
 	}
-	fmt.Println(a)
-	for i := n - 1; i > 0; i-- {
-		numi := a[i]
-		if numi >= 10 {
-			carry := numi / 10
-			a[i] = numi % 10
+	// fmt.Println(a)
+	for i := len(a) - 1; i > 0; i-- {
+		if a[i] >= 10 {
+			carry := a[i] / 10
+			a[i] = a[i] % 10
 			a[i-1] += carry
 		}
 	}
-	fmt.Println(a)
-	return ""
+	// fmt.Println(a)
+	res := ""
+	idx := 0
+	if a[0] == 0 {
+		idx = 1
+	}
+	for ; idx < len(a); idx++ {
+		res += strconv.Itoa(a[idx])
+	}
+	return res
 }
