e194a7fb38079fd6c9efa45da56bb424c6d171d7
zhangxq <zzzzail@163.com>
2023-01-06 10:08:27 +0800

    1

---
+++ rust/exercise/src/generic-fn.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // 这个是线程 ID
+    println!("process::id: {}", std::process::id());
+    println!("coin toss: {}", pick_one("heads", "tails"));
+    println!("cash prize: {}", pick_one(500, 1000));
+}
+
+fn pick_one<T>(a: T, b: T) -> T {
+    if std::process::id() % 2 == 0 { a } else { b }
+}
