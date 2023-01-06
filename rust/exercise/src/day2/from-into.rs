f948461b95008d6528f7f32614188d0ee7ddc6a8
zhangxq <zzzzail@163.com>
2023-01-06 21:10:07 +0800

    1

---
+++ rust/exercise/src/day2/from-into.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let s = String::from("hello");
+    let addr = std::net::Ipv4Addr::from([127, 0, 0, 1]);
+    let one = i16::from(true);
+    let bigger = i32::from(123i16);
+    println!("{s}, {addr}, {one}, {bigger}");
+
+    let s: String = "hello".into();
+    let addr: std::net::Ipv4Addr = [127, 0, 0, 1].into();
+    let one: i16 = true.into();
+    let bigger: i32 = 123i16.into();
+    println!("{s}, {addr}, {one}, {bigger}");
+}
