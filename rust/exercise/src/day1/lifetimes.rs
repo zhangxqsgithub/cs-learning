8d3cc1ddbb5936ce4ee8477895abce92431d2977
zhangxq <zzzzail@163.com>
2023-01-06 13:55:35 +0800

    1

--- rust/exercise/src/day1/lifetimes.rs
+++ rust/exercise/src/day1/lifetimes.rs
@@ -3,6 +3,13 @@ fn main() {
     let p2: Point = Point(20, 20);  // Put into different scope
     let p3: &Point = left_most(&p1, &p2);
     println!("left-most point: {:?}", p3);
+
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    // erase(text);
+    println!("{fox:?}");
+    println!("{dog:?}");
 }
 
 #[derive(Debug)]
@@ -19,4 +26,11 @@ struct Point(i32, i32);
 // The at least part is important when parameters are in different scopes.
 fn left_most<'a>(p1: &'a Point, p2: &'a Point) -> &'a Point {
     if p1.0 < p2.0 { p1 } else { p2 }
+}
+
+#[derive(Debug)]
+struct Highlight<'doc>(&'doc str);
+
+fn erase(text: String) {
+    println!("Bye {text}!");
 }
