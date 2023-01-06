856fb0389d1d0a195b68c9f1026fbbf64efa2147
zhangxq <zzzzail@163.com>
2023-01-06 20:46:55 +0800

    1

---
+++ rust/exercise/src/day3/trait.rs
@@ -0,0 +1,31 @@
+trait Greet {
+    fn say_hello(&self);
+}
+
+struct Dog {
+    name: String,
+}
+
+struct Cat;  // No name, cats won't respond to it anyway.
+
+impl Greet for Dog {
+    fn say_hello(&self) {
+        println!("Wuf, my name is {}!", self.name);
+    }
+}
+
+impl Greet for Cat {
+    fn say_hello(&self) {
+        println!("Miau!");
+    }
+}
+
+fn main() {
+    let pets: Vec<Box<dyn Greet>> = vec![
+        Box::new(Dog { name: String::from("Fido") }),
+        Box::new(Cat),
+    ];
+    for pet in pets {
+        pet.say_hello();
+    }
+}
