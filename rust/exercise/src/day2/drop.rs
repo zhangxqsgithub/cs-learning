3f31ed6e7db56305e4e80b770a4c8cf675b51166
zhangxq <zzzzail@163.com>
2023-01-08 18:41:03 +0800

    1

--- rust/exercise/src/day2/drop.rs
+++ rust/exercise/src/day2/drop.rs
@@ -1,13 +1,3 @@
-struct Droppable {
-    name: &'static str,
-}
-
-impl Drop for Droppable {
-    fn drop(&mut self) {
-        println!("Dropping {}", self.name);
-    }
-}
-
 fn main() {
     let a = Droppable { name: "a" };
     {
@@ -19,6 +9,19 @@ fn main() {
         }
         println!("Exiting block A");
     }
+    // 主动丢弃 a
     drop(a);
     println!("Exiting main");
 }
+
+struct Droppable {
+    name: &'static str,
+}
+
+// 当 c 和 d 被丢弃的时候，这个方法会触发
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.name);
+    }
+}
+
