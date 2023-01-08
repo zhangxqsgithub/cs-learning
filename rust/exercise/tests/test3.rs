556e75beb9f7a13d4f62d97cee17f3dac181c578
zhangxq <zzzzail@163.com>
2023-01-08 19:37:57 +0800

    1

---
+++ rust/exercise/tests/test3.rs
@@ -0,0 +1,11 @@
+/// 三条斜线的也是 Rust code，而且可以通过 cargo test 命令测试
+/// Shorten string will trip the string to the given length.
+///
+/// ```
+/// use playground::shorten_string;
+/// assert_eq!(shorten_string("Hello World", 5), "Hello");
+/// assert_eq!(shorten_string("Hello World", 20), "Hello World");
+/// ```
+pub fn shorten_string(s: &str, length: usize) -> &str {
+    &s[..std::cmp::min(length, s.len())]
+}
