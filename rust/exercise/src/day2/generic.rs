c6abc3262d8aa3ea1bd705e644af42f1e2d2ed27
zhangxq <zzzzail@163.com>
2023-01-08 18:48:52 +0800

    1

--- rust/exercise/src/day2/generic.rs
+++ rust/exercise/src/day2/generic.rs
@@ -1,6 +1,8 @@
 fn main() {
+    // 不用类型推断
     // let integer: Point<i32> = Point { x: 5, y: 10 };
     // let float: Point<f32> = Point { x: 1.0, y: 4.0 };
+    // 类型推断
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
     println!("{integer:?} and {float:?}");
