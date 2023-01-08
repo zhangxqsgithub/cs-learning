158b410f79f935fd514ba9c090807ff9ab963a35
zhangxq <zzzzail@163.com>
2023-01-08 19:55:39 +0800

    1

---
+++ rust/exercise/src/day4/channels.rs
@@ -0,0 +1,17 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    // 创建一个 channel
+    let (tx, rx) = mpsc::channel();
+
+    tx.send(10).unwrap();
+    tx.send(20).unwrap();
+
+    println!("Received: {:?}", rx.recv());
+    println!("Received: {:?}", rx.recv());
+
+    let tx2 = tx.clone();
+    tx2.send(30).unwrap();
+    println!("Received: {:?}", rx.recv());
+}
