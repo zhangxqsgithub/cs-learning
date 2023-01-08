b7fbe6ce3c12062323378da16e6a4fa753817d4a
zhangxq <zzzzail@163.com>
2023-01-08 18:48:33 +0800

    1

---
+++ rust/exercise/src/day2/generic.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // let integer: Point<i32> = Point { x: 5, y: 10 };
+    // let float: Point<f32> = Point { x: 1.0, y: 4.0 };
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("{integer:?} and {float:?}");
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+
