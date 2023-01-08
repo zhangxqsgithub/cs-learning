fdef27450776e111efff5d7592aba1531b0122b5
zhangxq <zzzzail@163.com>
2023-01-08 19:32:32 +0800

    1

---
+++ rust/exercise/tests/test.rs
@@ -0,0 +1,22 @@
+fn first_word(text: &str) -> &str {
+    match text.find(' ') {
+        Some(idx) => &text[..idx],
+        None => &text,
+    }
+}
+
+// 在 tests 文件夹下创建的测试方法，可以通过 cargo test 命令全部测试
+#[test]
+fn test_empty() {
+    assert_eq!(first_word(""), "");
+}
+
+#[test]
+fn test_single_word() {
+    assert_eq!(first_word("Hello"), "Hello");
+}
+
+#[test]
+fn test_multiple_words() {
+    assert_eq!(first_word("Hello World"), "Hello");
+}
