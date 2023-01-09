aa8718e173335c036bd0401438bcf8d01d36eae1
zhangxq <zzzzail@163.com>
2023-01-09 09:10:22 +0800

    1

--- rust/official-tour/src/main.rs
+++ rust/official-tour/src/main.rs
@@ -1,11 +1,15 @@
 use std::io;
 
 fn main() {
+    func_demo();
+}
+
+fn guess_game() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let some: String = String::from("x");   // 不可被修改
+    let _some: String = String::from("x");   // 不可被修改
     // new 是一个 String 结构体那定义的常量方法 pub const fn new() {...}
     // 访问常量方法仍需使用 :: 运算符
     let mut guess: String = String::new();  // 可被修改
@@ -18,4 +22,41 @@ fn main() {
         .expect("Failed to read line");
     
     println!("You guessed: {guess}");
+}
+
+fn func_demo() {
+    func1();
+
+    /*
+    带有参数的方法
+    调用 func2 方法时，param 参数被复制到 func2 方法定义的参数 x 上。
+    param 参数和 func2 的接收参数之间，所有权互不影响（因为是两个值）
+     */ 
+    let param: i32 = 100;
+    func2(param);
+    println!("param: {}", param);
+
+    // 传递两个参数的方法
+    func3(1, 'h');
+
+    // 语句（没有返回值）
+    let a = 100;
+    // 表达式（有返回值）
+    let b = {
+        let x = 100;
+        a + x + 1
+    };
+    println!("{a}, {b}")
+}
+
+fn func1() {
+    println!("func1");
+}
+
+fn func2(x: i32) {
+    println!("param: {}", x);
+}
+
+fn func3(val: i32, unit_label: char) {
+    println!("The measurement is: {val}{unit_label}");
 }
