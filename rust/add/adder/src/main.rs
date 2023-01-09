59a68b4eaf3bfe240223054bf1754fbc51e1eb4e
zhangxq <zzzzail@163.com>
2023-01-09 21:42:24 +0800

    1

---
+++ rust/add/adder/src/main.rs
@@ -0,0 +1,8 @@
+use add_one;
+
+fn main() {
+    println!("Hello, world!");
+
+    let num = 10;
+    println!("{}", add_one::add_one(num));
+}
--- rust/official-tour/src/main.rs
+++ rust/official-tour/src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+pub mod lib;
+
 fn main() {
     func_demo();
 }
