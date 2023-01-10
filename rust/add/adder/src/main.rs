8f486f8614543b570f37c8de281784e54844812b
zhangxq <zzzzail@163.com>
2023-01-10 08:52:52 +0800

    1

--- rust/add/adder/src/main.rs
+++ rust/add/adder/src/main.rs
@@ -1,4 +1,4 @@
-use add_one;
+use add_one::add_one;
 
 // 定义一个 module 名字为 garden1
 mod garden1;
@@ -13,5 +13,5 @@ fn main() {
     println!("Hello, world!");
 
     let num = 10;
-    println!("{}", add_one::add_one(num));
+    println!("{}", add_one(num));
 }
