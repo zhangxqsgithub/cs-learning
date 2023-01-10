bc0978e8f23675b0a482c9fed95ef11785505c2c
zhangxq <zzzzail@163.com>
2023-01-10 14:30:27 +0800

    1

--- rust/official-tour/src/smart-pointer.rs
+++ rust/official-tour/src/smart-pointer.rs
@@ -6,6 +6,9 @@ fn main() {
     println!("The longest string is {}", result);
 }
 
+// 生命周期的声明方式和范型一样 <'a>
+// 范型 <T>
+// 结合 <'a T>
 fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
     if str1.len() >= str2.len() { str1 } else { str2 }
 }
