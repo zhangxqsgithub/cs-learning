a297f4e7988d3e784a96407b87a430d117daafce
zhangxq <zzzzail@163.com>
2023-01-10 09:30:45 +0800

    1

--- rust/backyard/src/main.rs
+++ rust/backyard/src/main.rs
@@ -1,4 +1,5 @@
 use crate::garden::vegetables::Asparagus;
+use crate::a::b::add;
 
 pub mod garden;
 /*
@@ -6,10 +7,13 @@ pub mod garden;
     2. 文件 garden.rs 就是一个 module
     3. garden/mod.rs 也是一个 module
  */
+pub mod a;
 
 fn main() {
     println!("Hello, world!");
 
     let a: Asparagus = Asparagus {};
     println!("{a:?}");
+
+    println!("{}", add(1, 2));
 }
