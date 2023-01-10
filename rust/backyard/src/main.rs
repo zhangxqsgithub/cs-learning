cb109d4e548bd182b19e0449f1541a52d4033b60
zhangxq <zzzzail@163.com>
2023-01-10 08:43:00 +0800

    1

---
+++ rust/backyard/src/main.rs
@@ -0,0 +1,15 @@
+use garden::vegetables::Asparagus;
+
+pub mod garden;
+/*
+    1. mod xxx {}
+    2. 文件 garden.rs 就是一个 module
+    3. garden/mod.rs 也是一个 module
+ */
+
+fn main() {
+    println!("Hello, world!");
+
+    let a: Asparagus = Asparagus {};
+    println!("{a:?}");
+}
