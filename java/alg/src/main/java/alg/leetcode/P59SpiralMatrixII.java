ac6caa896bd2236712356742057c7a0651ada5ed
zhangxq <zzzzail@163.com>
2023-01-11 13:17:41 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P59SpiralMatrixII.java
@@ -0,0 +1,17 @@
+package alg.leetcode;
+
+/**
+ * 螺旋矩阵 II
+ *
+ * @author zhangxq
+ * @link https://leetcode.cn/problems/spiral-matrix-ii/
+ * @since 2023/1/11
+ */
+public class P59SpiralMatrixII {
+    
+    public int[][] generateMatrix(int n) {
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        return null;
+    }
+}
