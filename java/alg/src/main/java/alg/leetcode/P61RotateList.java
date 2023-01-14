12576fda99d9e976bcddba6eafcccb9e5084bd02
zhangxq <zzzzail@163.com>
2023-01-14 11:13:38 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P61RotateList.java
+++ java/alg/src/main/java/alg/leetcode/P61RotateList.java
@@ -14,8 +14,23 @@ public class P61RotateList {
     }
     
     public ListNode rotateRight(ListNode head, int k) {
-        
-        return null;
+        if (head == null) return null;
+        int n = 0;
+        ListNode tail = null; // 尾节点
+        // 链表有 n 个节点
+        for (ListNode p = head; p != null; p = p.next) {
+            tail = p;
+            n++;
+        }
+        // 求 k % n
+        k = k % n;
+        if (k == 0) return head;
+        ListNode p = head;
+        for (int i = 0; i < n - k - 1; i ++) p = p.next;
+        tail.next = head;
+        head = p.next;
+        p.next = null;
+        return head;
     }
     
     static class ListNode {
