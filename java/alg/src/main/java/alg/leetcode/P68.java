16b023857c65bd1a9447a6dd6277bf89a8edc50b
zhangxq <zzzzail@163.com>
2023-01-14 14:51:33 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P68.java
+++ java/alg/src/main/java/alg/leetcode/P68.java
@@ -1,5 +1,6 @@
 package alg.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +17,35 @@ public class P68 {
     }
     
     public List<String> fullJustify(String[] words, int maxWidth) {
-        
-        return null;
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            int j = i + 1;
+            int len = words[i].length();
+            while (j < words.length && len + 1 + words[j].length() <= maxWidth) {
+                len += 1 + words[j++].length();
+            }
+            String line = "";
+            if (j == words.length || j == i + 1) { // 左对齐
+                line += words[i];
+                for (int k = i + 1; k < j; k++) line += ' ' + words[k];
+                while (line.length() < maxWidth) line += ' ';
+            }
+            else { // 左右对齐
+                int cnt = j - i - 1, r = maxWidth - len + cnt;
+                line += words[i];
+                int k = 0;
+                while (k < r % cnt) {
+                    line += " ".repeat(r / cnt + 1) + words[i + k + 1];
+                    k++;
+                }
+                while (k < cnt) {
+                    line += " ".repeat(r / cnt) + words[i + k + 1];
+                    k++;
+                }
+            }
+            res.add(line);
+            i = j - 1;
+        }
+        return res;
     }
 }
