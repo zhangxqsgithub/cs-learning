3ea603348726ee89d89115b672fd6d8ab6e90974
zhangxq <zzzzail@163.com>
2023-04-26 08:30:07 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P101SymmetricTree.java
@@ -0,0 +1,38 @@
+package alg.leetcode;
+
+/**
+ * @link https://leetcode.cn/problems/symmetric-tree/
+ * @author zhangxq
+ * @since 2023/4/26
+ */
+public class P101SymmetricTree {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return dfs(root.left, root.right);
+    }
+    
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+    
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
