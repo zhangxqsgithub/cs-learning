8e4312339c4bbdc0059cbdf6c6fe4b82db8d7c31
zhangxq <zzzzail@163.com>
2023-05-07 15:17:06 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P171ExcelSheetColumnNumber.java
+++ java/alg/src/main/java/alg/leetcode/P171ExcelSheetColumnNumber.java
@@ -10,15 +10,12 @@ public class P171ExcelSheetColumnNumber {
     public static void main(String[] args) {
     
     }
-    
+
+    // 本质上就是 26 进制的数
     public int titleToNumber(String s) {
-        var a = 0;
-        for (int i = 0, p = 26; i < s.length() - 1; i++, p *= 26) {
-            a += p;
-        }
-        // 秦九韶 算法
-        int b = 0;
-        for (var c : s.toCharArray()) b = b * 26 + c - 'A';
-        return a + b + 1;
+        var res = 0;
+        for (var c : s.toCharArray())
+            res = res * 26 + c - 'A' + 1;
+        return res;
     }
 }
