9b57007a52b57c97d18c8ac36e87af5171d2ae70
zhangxq <zzzzail@163.com>
2023-05-08 16:19:18 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P210CourseScheduleII.java
@@ -0,0 +1,46 @@
+package alg.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+/**
+ * @link https://leetcode.cn/problems/course-schedule-ii/
+ * @author zhangxq
+ * @since 2023/5/8
+ */
+public class P210CourseScheduleII {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    public int[] findOrder(int n, int[][] edges) {
+        List<List<Integer>> g = new ArrayList<>();
+        for (var i = 0; i < n; i++) g.add(new ArrayList<>());
+        int[] d = new int[n];
+        for (var e : edges) {
+            int b = e[0], a = e[1];
+            g.get(a).add(b);
+            d[b]++;
+        }
+        
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 0; i < n; i++)
+            if (d[i] == 0)
+                que.add(i);
+        
+        var res = new int[n];
+        var cnt = 0;
+        while (!que.isEmpty()) {
+            var t = que.poll();
+            res[cnt++] = t;
+            for (var i : g.get(t))
+                if (--d[i] == 0)
+                    que.add(i);
+        }
+        return cnt == n ? res : new int[]{};
+    }
+}
