32780ed874217fa4e657f1dd4ac5db8cd8a91487
zhangxq <zzzzail@163.com>
2023-05-20 20:15:10 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P218TheSkylineProblem.java
+++ java/alg/src/main/java/alg/leetcode/P218TheSkylineProblem.java
@@ -1,10 +1,13 @@
 package alg.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
 
 /**
+ * 扫描线问题
+ *
  * @author zhangxq
  * @link https://leetcode.cn/problems/the-skyline-problem/
  * @since 2023/5/9
@@ -33,23 +36,21 @@ public class P218TheSkylineProblem {
         List<List<Integer>> res = new ArrayList<>();
         var points = new ArrayList<Point>();
         for (var b : buildings) {
+            // 左端点重合的话，高的在前低的在后；右端点重合的话，低的在前高的在后；左右重合的话，左在前右在后；
+            // 若是左端点存负数的高度
             points.add(new Point(b[0], -b[2]));
+            // 右端点存正数的高度
             points.add(new Point(b[1], b[2]));
         }
-        points.sort((a, b) -> {
-            if (a.x == b.x) return a.h - b.h;
-            else return a.x - b.x;
-        });
+        points.sort((a, b) -> a.x == b.x ? a.h - b.h : a.x - b.x);
+        // 保存当前长条内部所有的高度
         var tree = new TreeMap<Integer, Integer>();
         tree.put(0, 0);
         for (var p : points) {
             int x = p.x, h = Math.abs(p.h);
             if (p.h < 0) { // 左端点
                 if (h > tree.lastKey()) {
-                    var cur = new ArrayList<Integer>();
-                    cur.add(x);
-                    cur.add(h);
-                    res.add(cur);
+                    res.add(List.of(x, h));
                 }
                 tree.put(h, tree.getOrDefault(h, 0) + 1);
             }
@@ -57,12 +58,8 @@ public class P218TheSkylineProblem {
                 var cnt = tree.get(h) - 1;
                 if (cnt == 0) tree.remove(h);
                 else tree.put(h, cnt);
-                
                 if (h > tree.lastKey()) {
-                    var cur = new ArrayList<Integer>();
-                    cur.add(x);
-                    cur.add(tree.lastKey());
-                    res.add(cur);
+                    res.add(List.of(x, tree.lastKey()));
                 }
             }
         }
@@ -71,7 +68,6 @@ public class P218TheSkylineProblem {
     
     static class Point {
         int x, h;
-        
         Point(int x, int h) {
             this.x = x;
             this.h = h;
