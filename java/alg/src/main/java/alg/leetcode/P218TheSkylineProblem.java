5f89dbbf5cc2a78183ebee08a9980b883af03e70
zhangxq <zzzzail@163.com>
2023-05-20 18:41:51 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P218TheSkylineProblem.java
+++ java/alg/src/main/java/alg/leetcode/P218TheSkylineProblem.java
@@ -31,8 +31,8 @@ public class P218TheSkylineProblem {
     
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Point> points = new ArrayList<>();
-        for (int[] b : buildings) {
+        var points = new ArrayList<Point>();
+        for (var b : buildings) {
             points.add(new Point(b[0], -b[2]));
             points.add(new Point(b[1], b[2]));
         }
@@ -40,13 +40,13 @@ public class P218TheSkylineProblem {
             if (a.x == b.x) return a.h - b.h;
             else return a.x - b.x;
         });
-        TreeMap<Integer, Integer> tree = new TreeMap<>();
+        var tree = new TreeMap<Integer, Integer>();
         tree.put(0, 0);
-        for (Point p : points) {
+        for (var p : points) {
             int x = p.x, h = Math.abs(p.h);
             if (p.h < 0) { // 左端点
                 if (h > tree.lastKey()) {
-                    List<Integer> cur = new ArrayList<>();
+                    var cur = new ArrayList<Integer>();
                     cur.add(x);
                     cur.add(h);
                     res.add(cur);
@@ -54,12 +54,12 @@ public class P218TheSkylineProblem {
                 tree.put(h, tree.getOrDefault(h, 0) + 1);
             }
             else {
-                int cnt = tree.get(h) - 1;
+                var cnt = tree.get(h) - 1;
                 if (cnt == 0) tree.remove(h);
                 else tree.put(h, cnt);
                 
                 if (h > tree.lastKey()) {
-                    List<Integer> cur = new ArrayList<>();
+                    var cur = new ArrayList<Integer>();
                     cur.add(x);
                     cur.add(tree.lastKey());
                     res.add(cur);
