4770e3f3948ccd5a617e4dbb5ebbd2b7c3e6f092
zhangxq <zzzzail@163.com>
2023-05-22 11:41:20 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P224BasicCalculator.java
@@ -0,0 +1,18 @@
+package alg.leetcode;
+
+/**
+ * @link https://leetcode.cn/problems/basic-calculator/
+ * @author zhangxq
+ * @since 2023/5/22
+ */
+public class P224BasicCalculator {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    public int calculate(String s) {
+        
+        return 0;
+    }
+}
--- java/alg/src/main/java/alg/leetcode/P65ValidNumber.java
+++ java/alg/src/main/java/alg/leetcode/P65ValidNumber.java
@@ -13,38 +13,31 @@ public class P65ValidNumber {
     
     }
     
-    // 这个解法是错误的
     public boolean isNumber(String s) {
-        // 去掉前后空格
         s = s.trim();
-        char[] a = s.toCharArray();
-        int n = s.length();
-        if (n == 0) return false;
-        // 去掉前面正负号
-        if (a[0] == '+' || a[0] == '-') s = s.substring(1);
-        a = s.toCharArray();
-        n = a.length;
-        if (n == 0) return false;
-        if (a[0] == '.' && (n == 1 || a[1] == 'e' || a[1] == 'E')) return false;
+        if (s.length() == 0) return false;
+        // 开头结尾是 +- 号的
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') s = s.substring(1);
+        if (s.isEmpty()) return false;
+        if (s.charAt(0) == '.' && (s.length() == 1 || s.charAt(1) == 'e' || s.charAt(1) == 'E')) return false;
         // 记录 . 和 e 出现的次数
         int dot = 0, e = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] == '.') {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '.') {
                 if (dot > 0 || e > 0) return false;
                 dot++;
             }
-            else if (a[i] == 'e' || a[i] == 'E') {
-                if (i != 0 || i + 1 == n || e > 0) return false;
-                if (a[i + 1] == '+' || a[i + 1] == '-') {
-                    if (i + 2 == n) return false;
-                    // 把正负号跳过
-                    i++;
+            else if (c == 'e' || c == 'E') {
+                if (i == 0 || i + 1 == s.length() || e > 0) return false;
+                if (s.charAt(i + 1) == '+' || s.charAt(i + 1) == '-') {
+                    if (i + 2 == s.length()) return false;
+                    i++; // 跳过正负号
                 }
                 e++;
             }
-            else if (a[i] < '0' || a[i] > '9') {
-                return false;
-            }
+            // 字符不是阿拉伯数字
+            else if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
         }
         return true;
     }
