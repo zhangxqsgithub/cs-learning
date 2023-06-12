5fa1361ab56fc97a2b11154e526a99e3ec56c5c1
zhangxq <zzzzail@163.com>
2023-06-12 11:32:24 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P449SerializeAndDeserializeBst.java
@@ -0,0 +1,66 @@
+package alg.leetcode;
+
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhangxq
+ * @link https://leetcode.cn/problems/serialize-and-deserialize-bst/
+ * @since 2023/6/12
+ */
+public class P449SerializeAndDeserializeBst {
+    
+    public static void main(String[] args) {
+        var solution = new Codec();
+        var root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        var res = solution.serialize(root);
+        System.out.println(res);
+        
+        var res2 = solution.deserialize(res);
+    }
+    
+    static
+    public class Codec {
+        public String serialize(TreeNode root) {
+            var res = new StringBuilder();
+            dfs_s(root, res);
+            return res.toString();
+        }
+        public void dfs_s(TreeNode root, StringBuilder sb) {
+            if (root == null) return;
+            sb.append(root.val).append(' ');
+            dfs_s(root.left, sb);
+            dfs_s(root.right, sb);
+        }
+        public TreeNode deserialize(String data) {
+            if (data.isBlank()) return null;
+            var list = new ArrayList<Integer>();
+            var arr = data.split("\\s+");
+            for (var str : arr) list.add(Integer.parseInt(str.trim()));
+            return dfs_d(list, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        int u = 0;
+        public TreeNode dfs_d(List<Integer> data, int min, int max) {
+            if (u == data.size() || data.get(u) < min || data.get(u) > max) return null;
+            var root = new TreeNode(data.get(u));
+            u++;
+            root.left = dfs_d(data, min, root.val);
+            root.right = dfs_d(data, root.val, max);
+            return root;
+        }
+    }
+    
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
