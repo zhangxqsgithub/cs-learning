97dbc0262917c4233ecd7d729931d7a025cb44fb
zhangxq <zzzzail@163.com>
2023-06-19 08:52:13 +0800

    1

--- java/alg/src/main/java/alg/acwing/algbase/C1_1_Sort.java
+++ java/alg/src/main/java/alg/acwing/algbase/C1_1_Sort.java
@@ -11,7 +11,7 @@ public class C1_1_Sort {
     public static void main(String[] args) {
         var solution = new C1_1_Sort();
         var arr = new int[]{3, 1, 4, 2, 5,};
-        solution.quickSort(arr, 0, arr.length - 1);
+        solution.quickSort2(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
     
@@ -26,10 +26,8 @@ public class C1_1_Sort {
      * 方法
      * 1. 额外空间法，申请两个数组，一个存放 >= pivot 的数，一个存放 <= pivot 的数，完成之后将两个数组合并到原
      * 数组中。
-     * 2. 双指针法，前后指针同时往前走，前面指针直到遇到 > x 的数停止，有面指针遇到 < x 的数停止，若前面指针比后
-     * 面指针小，则交换两个值；若前面指针比后面指针大，则前面指针会停到刚好 > x 的数上，下一步将前面指针的值和 x
-     * 交换，此时数组 [l, i - 1] 是前半部分需要排序的数组 [i + 1, r] 是后半部分需要排序的数组。
-     * 相遇则
+     * 2. 双指针法，前后指针 i 和 j 同时往前走，前面指针直到遇到 > x 的数停止，有面指针遇到 < x 的数停止，若前面指针比后
+     * 面指针小，则交换两个值。[l, i - 1] 区间是 <= x 的数，[j + 1, r] 区间是 >= x 的数。
      * 3. 三指针法，将数组分为三个部分，[l, lt] 是严格小于 x 的区间，[lt + 1, i] 是等于 x 的区间，[gt, r] 是
      * 严格大于 x 的区间。
      * 定义好 lt、i、gt 三个参数即可。
@@ -51,8 +49,18 @@ public class C1_1_Sort {
         quickSort(arr, gt, r);
     }
     
-    
+    // 代码简洁！（但这种方法并没有有效地利用 pivot，省略一个排序的数字）
     public void quickSort2(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int pivot = arr[l];
+        int i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (arr[i] < pivot); // i 指针会停在 <= pivot 的数字上
+            do j--; while (arr[j] > pivot); // j 指针会停在 >= pivot 的数字上
+            if (i < j) swap(arr, i, j);
+        }
+        quickSort2(arr, l, j);
+        quickSort2(arr, j + 1, r);
     }
     
     // 归并排序
