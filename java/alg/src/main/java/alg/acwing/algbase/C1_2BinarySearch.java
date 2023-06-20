50856d31d8e8310fa73fdb5c0c65b7288860984f
zhangxq <zzzzail@163.com>
2023-06-20 08:39:09 +0800

    1

--- java/alg/src/main/java/alg/acwing/algbase/C1_2BinarySearch.java
+++ java/alg/src/main/java/alg/acwing/algbase/C1_2BinarySearch.java
@@ -12,7 +12,10 @@ public class C1_2BinarySearch {
         // var res = solution.binarySearch(arr, -1);
         // System.out.println(res);
         
-        var res = solution.lowerBound(arr, 4);
+        // var res = solution.lowerBound(arr, 4);
+        // System.out.println(res);
+        
+        var res = solution.test(arr, -100_000);
         System.out.println(res);
     }
     
@@ -22,7 +25,7 @@ public class C1_2BinarySearch {
      */
     public boolean binarySearch(int[] arr, int target) {
         int l = 0, r = arr.length - 1;
-        while (l <= r) {
+        while (l <= r) { // 这里用 l <= r 的原因是内部需要判断 arr[mid] == target
             int mid = l + (r - l) / 2;
             if (arr[mid] == target) return true;
             else if (arr[mid] < target) l = mid + 1;
@@ -62,6 +65,26 @@ public class C1_2BinarySearch {
         return l;
     }
     
+    /**
+     * 二分查找，l 下标收敛到 >= target 的第一个下标中。
+     * 当遇到严格小于 target 的时候，可以直接放弃左半部分的值；遇到 arr[mid] >= target 的时候说明 arr[mid] 之前
+     * 可能还有 >= target 值的下标存在，所以设置 r = mid;
+     * 这样即使 [l, mid - 1] 之间不存在 >= target 值的下标，arr[mid] 也是满足条件的。
+     * 所以，此二分一定收敛到 l = r 或 l + 1 = r 的两种情况中，当 l = r 是就结束了。
+     * 情况 1：arr[l] == arr[r] 且 arr[l] >= target; mid 的值为 l; r = mid; 则会收敛到 l = r 的情况。
+     * 情况 2：arr[l] < arr[r] 且 arr[l] < target; mid 的值为 l; l = mid + 1; 即 l = r 的情况。
+     * 情况 3：arr[l] < arr[r] 且 arr[l] >= target; mid 的值为 l; r = l = mid; 即 l = r 的情况。
+     */
+    public int lowerBound2(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] < target) l = mid + 1;
+            else r = mid; // arr[mid] >= target
+        }
+        return l;
+    }
+    
     // 二分查找法, 实现upper_bound
     // 即在一个有序数组arr中, 寻找大于target的元素的第一个索引
     // 如果存在, 则返回相应的索引index
@@ -73,6 +96,28 @@ public class C1_2BinarySearch {
             if (arr[mid] <= target) l = mid + 1;
             else r = mid; // nums[mid] > target
         }
+        return l;
+    }
+    
+    public int upperBound2(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] <= target) l = mid + 1;    // 需要丢弃的区间
+            else r = mid;                           // 需要保留的区间
+        }
+        return l;
+    }
+    
+    public int test(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] < target) l = mid + 1;
+            else r = mid; // arr[mid] >= target
+        }
+        System.out.println(l);
+        System.out.println(r);
         return l;
     }
 }
