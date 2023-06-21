ad8cb5570c47be80f39a2c07aa9050b5698a0814
zhangxq <zzzzail@163.com>
2023-06-21 08:18:32 +0800

    1

--- java/alg/src/main/java/alg/acwing/algbase/C1_5DoublePointer.java
+++ java/alg/src/main/java/alg/acwing/algbase/C1_5DoublePointer.java
@@ -32,6 +32,21 @@ public class C1_5DoublePointer {
         System.out.println(1 | 0);
         System.out.println(1 & 0);
         System.out.println(1 ^ 0);
+        
+        int n = 48, k = 3;
+        // 求 n 的第 k 位数字
+        System.out.println(n >> k & 1);
+        // 返回 n 的最后一位 1 和后面的 0 组成的数字：lowbit 方法
+        // 1010    返回 10
+        // 1010000 返回 10000
+        System.out.println(n & -n);
+        
+        printBinary(48);
+        printBinary(-48); // 负数用补码表示，原码 -> 反码 通过每位二进制取反获得，反码再 + 1 得到补码。
+    }
+    public void printBinary(int x) {
+        for (int k = 31; k >= 0; k--) System.out.print(x >> k & 1);
+        System.out.println();
     }
     // 离散化
     // 区间合并
