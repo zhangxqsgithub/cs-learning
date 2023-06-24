11c58ffdf40e657864f284a47e89b64d705f8a30
zhangxq <zzzzail@163.com>
2023-06-24 17:39:03 +0800

    1

--- rust/alg/src/main.rs
+++ rust/alg/src/main.rs
@@ -1,3 +1,5 @@
+pub mod leetcode;
+
 fn main() {
     println!("Hello, world!");
 }
--- rust/backyard/src/main.rs
+++ rust/backyard/src/main.rs
@@ -6,6 +6,10 @@ pub mod garden;
     1. mod xxx {}
     2. 文件 garden.rs 就是一个 module
     3. garden/mod.rs 也是一个 module
+
+    module 在 rust 中的应用：
+        1. 在 main.rs 中声明一个 mod，语法：`pub mod xxx;`
+        2. 创建文件夹 xxx，并且在文件夹内创建 mod.rs 文件
  */
 pub mod a;
 
