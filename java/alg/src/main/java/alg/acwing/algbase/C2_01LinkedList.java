99644ba2bffd2b15b1b71ffd16e6385140f29117
zhangxq <zzzzail@163.com>
2023-06-25 09:10:57 +0800

    1

--- java/alg/src/main/java/alg/acwing/algbase/C2_01LinkedList.java
+++ java/alg/src/main/java/alg/acwing/algbase/C2_01LinkedList.java
@@ -108,5 +108,52 @@ public class C2_01LinkedList {
             return idx < 0;
         }
     }
-
+    
+    // 队列
+    static class Queue {
+        int[] q;
+        int head;
+        int tail;
+        public Queue(int n) {
+            q = new int[n];
+            head = 0;
+            tail = -1;
+        }
+        public void add(int x) { q[++tail] = x; }
+        public int peek() { return q[head]; }
+        public int poll() {
+            int t = q[head];
+            head++;
+            return t;
+        }
+        public boolean empty() {
+            return head > tail;
+        }
+    }
+    
+    // 循环队列
+    static class CircleQueue {
+        int[] q;
+        int head;
+        int tail;
+        public CircleQueue(int n) {
+            q = new int[n];
+            head = 0;
+            tail = -1;
+        }
+        public void add(int x) {
+            q[++tail] = x;
+            if (tail == q.length) tail = 0;
+        }
+        public int peek() { return q[head]; }
+        public int poll() {
+            int t = q[head];
+            head++;
+            if (head == q.length) head = 0;
+            return t;
+        }
+        public boolean empty() {
+            return head == tail;
+        }
+    }
 }
