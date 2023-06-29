609089cf0aa38ffd13ed1b591f4b9dfc087674a5
zhangxq <zzzzail@163.com>
2023-06-29 08:17:55 +0800

    1

--- java/alg/src/main/java/alg/acwing/P831KMP.java
+++ java/alg/src/main/java/alg/acwing/P831KMP.java
@@ -19,17 +19,19 @@ public class P831KMP {
         var s = br.readLine();
         
         var res = kmp(s, p);
-        for (Integer idx : res) {
-            System.out.print(idx + " ");
-        }
+        
+        // 这里不要每一行都调用 sout，会超时！
+        var sb = new StringBuilder();
+        for (Integer idx : res) sb.append(idx).append(" ");
+        System.out.println(sb);
     }
     
     // next 数组
     public static int[] next(String p) {
-        var next = new int[p.length() + 1];
-        for (int i = 2, j = 0; i <= p.length(); i++) {
-            while (j != 0 && p.charAt(i) != p.charAt(j + 1)) j = next[j];
-            if (p.charAt(i) == p.charAt(j + 1)) j++;
+        var next = new int[p.length()];
+        for (int i = 1, j = 0; i < p.length(); i++) {
+            while (j > 0 && p.charAt(i) != p.charAt(j)) j = next[j - 1];
+            if (p.charAt(i) == p.charAt(j)) j++;
             next[i] = j;
         }
         return next;
@@ -38,11 +40,11 @@ public class P831KMP {
     public static List<Integer> kmp(String s, String p) {
         var res = new ArrayList<Integer>();
         var next = next(p);
-        for (int i = 1, j = 0; i <= s.length(); i++) {
-            while (j > 0 && s.charAt(i) != p.charAt(j + 1)) j = next[j];
-            if (s.charAt(i) == p.charAt(j + 1)) j++;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != p.charAt(j)) j = next[j - 1];
+            if (s.charAt(i) == p.charAt(j)) j++;
             if (j == p.length()) {
-                j = next[j];
+                j = next[j - 1];
                 // 匹配成功
                 res.add(i - p.length() + 1);
             }
