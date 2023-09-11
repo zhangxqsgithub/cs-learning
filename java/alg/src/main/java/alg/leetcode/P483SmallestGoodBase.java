133d537c15e166312cab686aea51aafd6e1c1b22
zhangxq <zzzzail@163.com>
2023-09-11 18:37:35 +0800

    1

---
+++ java/alg/src/main/java/alg/leetcode/P483SmallestGoodBase.java
@@ -0,0 +1,17 @@
+package alg.leetcode;
+
+/**
+ * @link https://leetcode.cn/problems/smallest-good-base/
+ * @author zhangxq
+ * @since 2023/9/11
+ */
+public class P483SmallestGoodBase {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    public String smallestGoodBase(String n) {
+        return "";
+    }
+}
