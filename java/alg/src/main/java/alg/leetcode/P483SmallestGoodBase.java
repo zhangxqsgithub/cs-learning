692f59e4e1ed5bbe040c5cb2d9a7db6002a2c3ae
zhangxq <zzzzail@163.com>
2023-09-12 10:28:41 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P483SmallestGoodBase.java
+++ java/alg/src/main/java/alg/leetcode/P483SmallestGoodBase.java
@@ -12,6 +12,18 @@ public class P483SmallestGoodBase {
     }
     
     public String smallestGoodBase(String n) {
-        return "";
+        long num = Long.parseLong(n);
+        for (int t = (int) log2(num) + 1; t >= 3; t--) {
+            long k = (long) Math.pow(num, 1.0 / (t - 1));
+            long r = 0;
+            // 求解 t 是不是一个答案
+            for (int i = 0; i < t; i++) r = r * k + 1;
+            if (r == num) return String.valueOf(k);
+        }
+        return String.valueOf(num - 1);
+    }
+    
+    public static double log2(long x) {
+        return Math.log(x) / Math.log(2);
     }
 }
