187e3aa750b31e4c08e5060f8877c5c2a8ede811
zhangxq <zzzzail@163.com>
2023-09-13 21:39:36 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P494TargetSum.java
+++ java/alg/src/main/java/alg/leetcode/P494TargetSum.java
@@ -28,4 +28,19 @@ public class P494TargetSum {
         dfs(nums, idx + 1, sum - num);
         dfs(nums, idx + 1, sum + num);
     }
+    
+    // 用动态规划解决
+    public int findTargetSumWays2(int[] nums, int target) {
+        if (target < -1000 || target > 1000) return 0;
+        int n = nums.length, offset = 1000;
+        var dp = new int[n + 1][2001];
+        dp[0][offset] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = -1000; j <= 1000; j++) {
+                if (j - nums[i - 1] >= -1000) dp[i][j + offset] += dp[i - 1][j - nums[i - 1] + offset];
+                if (j + nums[i - 1] <= 1000) dp[i][j + offset] += dp[i - 1][j + nums[i - 1] + offset];
+            }
+        }
+        return dp[n][target + offset];
+    }
 }
