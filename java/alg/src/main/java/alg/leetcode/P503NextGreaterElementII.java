47f0a05c1ea44444f29b85bb32c37e6d1e203912
zhangxq <zzzzail@163.com>
2023-09-17 11:46:27 +0800

    1

--- java/alg/src/main/java/alg/leetcode/P503NextGreaterElementII.java
+++ java/alg/src/main/java/alg/leetcode/P503NextGreaterElementII.java
@@ -1,5 +1,7 @@
 package alg.leetcode;
 
+import java.util.Stack;
+
 /**
  * @link https://leetcode.cn/problems/next-greater-element-ii/
  * @author zhangxq
@@ -11,8 +13,26 @@ public class P503NextGreaterElementII {
     
     }
     
+    /**
+     * 利用「破环成链技巧」，即将原数组复制一份放在后面即可。
+     */
     public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        var copy = new int[n * 2];
+        System.arraycopy(nums, 0, copy, 0, n);
+        System.arraycopy(nums, 0, copy, n, n);
         
-        return null;
+        var res = new int[n];
+        var stk = new Stack<Integer>();
+        for (int i = copy.length - 1; i >= 0; i--) {
+            int num = copy[i];
+            while (!stk.isEmpty() && stk.peek() <= num) stk.pop();
+            if (i < n) {
+                if (stk.isEmpty()) res[i] = -1;
+                else res[i] = stk.peek();
+            }
+            stk.push(num);
+        }
+        return res;
     }
 }
