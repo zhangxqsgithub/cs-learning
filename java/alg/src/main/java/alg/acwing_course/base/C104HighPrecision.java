56505df5f589e29511ddedc7b6a7413280df0136
will <zzzzail@163.com>
2024-03-26 16:59:38 +0800

    1

--- java/alg/src/main/java/alg/acwing_course/base/C104HighPrecision.java
+++ java/alg/src/main/java/alg/acwing_course/base/C104HighPrecision.java
@@ -16,6 +16,10 @@ public class C104HighPrecision {
         // 321 + 987
         var res1 = solution.add2(Arrays.asList(1, 2, 3), Arrays.asList(7, 8, 9));
         System.out.println(res1);
+        
+        // 99876 - 99321
+        var res2 = solution.sub(Arrays.asList(6, 7, 8, 9, 9), Arrays.asList(1, 2, 3, 9, 9));
+        System.out.println(res2);
     }
     
     public List<Integer> add(List<Integer> num1, List<Integer> num2) {
@@ -51,6 +55,38 @@ public class C104HighPrecision {
             t /= 10;
         }
         if (t != 0) res.add(t);
+        return res;
+    }
+    
+    /**
+     * 比较两个数的大小
+     * num1 < num2 返回 -1
+     * num1 == num2 返回 0
+     * num1 > num2 返回 1
+     */
+    public int compare(List<Integer> num1, List<Integer> num2) {
+        if (num1.size() != num2.size()) return num1.size() < num2.size() ? -1 : 1;
+        int n = num1.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (num1.get(i).equals(num2.get(i))) continue;
+            return num1.get(i) < num2.get(i) ? -1 : 1;
+        }
+        return 0;
+    }
+    
+    public List<Integer> sub(List<Integer> num1, List<Integer> num2) {
+        int n = num1.size();
+        var res = new ArrayList<Integer>();
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            t = num1.get(i) - t;
+            if (i < num2.size()) t -= num2.get(i);
+            res.add((t + 10) % 10);
+            if (t < 0) t = 1;
+            else t = 0;
+        }
+        // 将前导零去掉
+        while (!res.isEmpty() && res.getLast() == 0) res.removeLast();
         return res;
     }
 }
