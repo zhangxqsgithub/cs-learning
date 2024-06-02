1509f6f93993979c0fbf964ce555c3661c0f0226
will <zzzzail@163.com>
2024-06-02 23:26:03 +0800

    1

---
+++ java/alg/src/main/java/alg/acwing/P187.java
@@ -0,0 +1,15 @@
+package alg.acwing;
+
+/**
+ * 导弹防御系统
+ *
+ * @link https://www.acwing.com/problem/content/189/
+ * @author will
+ * @since 2024/6/2
+ */
+public class P187 {
+    
+    public static void main(String[] args) {
+    
+    }
+}
